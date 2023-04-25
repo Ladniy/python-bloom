6bfd05409077215266a13793cbcc004332c9d460
Ladniy <ladniy101@gmail.com>
2023-04-25 11:24:43 +0800

    Wrote a mini game to guess a prime number

---
+++ my-own-blooming/prime-guess-game/prime-guess-game.py
@@ -0,0 +1,45 @@
+import math
+from random import random
+
+
+def get_random_number(min=1, max=100):
+    '''
+    Returns random number in optional range
+    default is min = 1 and max = 100.
+    '''
+    number = math.floor(random() * (max - min + 1)) + min
+    return number
+
+
+def is_number_prime(number):
+    '''
+    Determines whether a number is prime
+    in a O(sqrt(n)) complexity time and
+    return boolean True or False.
+    '''
+    if number <= 1:
+        return False
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+
+def main():
+    print("Welcome to the Prime Guess Game!")
+    username = input("May i have your name? ")
+    print("Hello, %s!" % username)
+    print("Answer 'yes' if the number is prime, otherwise answer 'no'")
+
+    for i in range(0, 3):
+        random_number = get_random_number(1, 100)
+        true_answer = 'yes' if is_number_prime(random_number) else 'no'
+        user_answer = input("Is %s a prime number?\n" % random_number)
+        if user_answer != true_answer:
+            print("Try again!")
+            quit()
+        print("Good job!")
+
+
+if __name__ == '__main__':
+    main()
